0c75acfd73d6c286a7fc9b909842e6a3c4e40612
MENGTONG SREAN <117432545+mengtongsrean@users.noreply.github.com>
2024-01-13 15:29:45 +0700

    Add files

---
+++ blackJack.js
@@ -0,0 +1,93 @@
+// let player = {
+//     name: "tong",
+//     chips: "160",
+// }
+
+// let cards = []
+// let sum = 0
+// let hasBlackJack = false
+// let isAlive = false
+// let message = ""
+// let messageEl = document.getElementById("message-el")
+// let sumEl = document.getElementById("sum-el")
+// let cardsEl = document.getElementById("cards-el")
+// let playerEl = document.getElementById("player-el")
+
+
+
+// playerEl.textContent = player.name + ": $" + player.chips
+
+// function getRandomCard() {
+//     let randomCard = Math.floor( Math.random() * 13) + 1
+//     if (randomCard === 1) {
+//         return 11
+//     }
+//     if (randomCard > 10 && randomCard < 14) {
+//         return 10
+//     }
+//     return randomCard
+// }
+
+// function renderGame() {
+//     cardsEl.textContent = "cards: "
+//     for (i = 0; i < cards.length; i++) {
+//         cardsEl.textContent += cards[i] + " "
+//     }
+//     sumEl.textContent = "Sum: " + sum
+//     if (sum <= 20) {
+//         message = "Do you want to draw a new card?"
+//     } else if (sum === 21) {
+//         message = "You've got Blackjack!"
+//         hasBlackJack = true
+//     } else {
+//         message = "You're out of the game!"
+//         isAlive = false
+//     }
+//     messageEl.textContent = message
+// }
+// function startGame() {
+//     let firstCard = getRandomCard()
+//     let secondCard = getRandomCard()
+//     cards = [firstCard, secondCard]
+//     sum = firstCard + secondCard
+//     isAlive = true;
+//     renderGame()
+// }
+
+// function newCard() {
+//     if(isAlive && !hasBlackJack) {
+//         let card = getRandomCard()
+//         sum += card
+//         cards.push(card)
+//         renderGame()
+//     }
+
+// }
+
+// // 
+
+
+// let airBnb = {
+//     title: "Private Room in Galway",
+//     isAvailable:true,
+//     rating: 4.5,
+//     bedroom: 4,
+//     picture: [1, 2]
+
+// }
+
+// let largeCountries = ["China","India","USA","Indonesia","Pakistan"]
+
+
+let largeCountries = ["Tuvalu","India","USA","Indonesia","Monaco"]
+
+largeCountries.shift()
+largeCountries.unshift("china")
+
+largeCountries.pop()
+largeCountries.push("Pakistan")
+
+console.log(largeCountries)
+// console.log(largeCountries.unshift("china"))
+// console.log(largeCountries)
+
